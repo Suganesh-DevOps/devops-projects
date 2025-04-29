1d9e7e65edc54bec762b1c8d6d545dfffebe098d
root <root@DESKTOP-BA3EH26>
2025-04-29 22:16:28 +0530

    adding sql notes for learning purpose

---
+++ sql-notes.sql
@@ -0,0 +1,22 @@
+
+SQL basics
+
+1. select * from (table_name)
+
+2. select column1 , column2 from table_name;
+
+3. select * from table_name where country = 'USA';
+
+4. select * from table_name where id  > 50;
+
+5. INSERT INTO table_name (column1, column2) values (value1, value2);
+
+6. UPDATE - Modify existing records
+   UPDATE table_name SET column1 = value1 WHERE condition;
+   UPDATE products SET price = 29.99 WHERE product_id = 101;
+
+7. DELETE - Remove records
+   DELETE FROM table_name WHERE condition;
+   DELETE FROM orders WHERE order_id = 1005;
+
+8. 
